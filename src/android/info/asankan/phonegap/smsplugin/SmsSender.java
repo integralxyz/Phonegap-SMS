cd6573823c91577c65558556f17d769c255483d4
Integral.XYZ <31256139+integralxyz@users.noreply.github.com>
2018-07-08 20:01:03 +0200

    Update SmsSender.java

--- src/android/info/asankan/phonegap/smsplugin/SmsSender.java
+++ src/android/info/asankan/phonegap/smsplugin/SmsSender.java
@@ -54,11 +54,11 @@ public class SmsSender {
         */
         ArrayList<PendingIntent> sentPendingIntents = new ArrayList<PendingIntent>();
         ArrayList<PendingIntent> deliveredPendingIntents = new ArrayList<PendingIntent>();
-        PendingIntent sentPI = PendingIntent.getBroadcast(mContext, 0,
-                new Intent(mContext, SmsSentReceiver.class), 0);
+        PendingIntent sentPI = PendingIntent.getBroadcast(getApplicationContext( ), 0,
+                new Intent(getApplicationContext( ), SmsSentReceiver.class), 0);
 
-        PendingIntent deliveredPI = PendingIntent.getBroadcast(mContext, 0,
-                new Intent(mContext, SmsDeliveredReceiver.class), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(getApplicationContext( ), 0,
+                new Intent(getApplicationContext( ), SmsDeliveredReceiver.class), 0);
         try {
             SmsManager sms = SmsManager.getDefault();
             ArrayList<String> mSMSMessage = sms.divideMessage(message);
